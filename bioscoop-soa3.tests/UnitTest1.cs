2a0bbea17c08279f4400bedc1eea0a7a7404c42f
studentThomas <117987244+studentThomas@users.noreply.github.com>
2025-02-07 12:35:40 +0100

    feat: add basic unit tests

--- bioscoop-soa3.tests/UnitTest1.cs
+++ bioscoop-soa3.tests/UnitTest1.cs
@@ -1,15 +1,97 @@
-﻿namespace bioscoop_soa3.tests;
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using bioscoop_soa3;
 
-public class Tests
+namespace bioscoop_soa3.tests
 {
-    [SetUp]
-    public void Setup()
+    public class OrderTests
     {
-    }
+        [Test]
+        public void CalculatePrice_NoTickets_ShouldReturnZero()
+        {
+            // Arrange
+            Order order = new(321);
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            // Act
+            double totalPrice = order.CalculatePrice();
+
+            // Assert
+            Assert.That(totalPrice, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CalculatePrice_OnlyStudentTickets_ShouldReturnCorrectTotal()
+        {
+            // Arrange
+            Movie movie = new("Bliep bloep");
+            MovieScreening movieScreening = new(movie, DateTime.MinValue, 10);
+            Order order = new(321);
+
+            order.AddSeatReservation(new MovieTicket(movieScreening, true, false, 12, 12));
+            order.AddSeatReservation(new MovieTicket(movieScreening, true, false, 12, 12));
+
+            // Act
+            double totalPrice = order.CalculatePrice();
+
+            // Assert
+            Assert.That(totalPrice, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void CalculatePrice_OnlyNonStudentTickets_ShouldReturnCorrectTotal()
+        {
+            // Arrange
+            Movie movie = new("Bliep bloep");
+            MovieScreening movieScreening = new(movie, DateTime.MinValue, 10);
+            Order order = new(321);
+
+            order.AddSeatReservation(new MovieTicket(movieScreening, false, false, 12, 12));
+            order.AddSeatReservation(new MovieTicket(movieScreening, false, false, 12, 12));
+
+            // Act
+            double totalPrice = order.CalculatePrice();
+
+            // Assert
+            Assert.That(totalPrice, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void CalculatePrice_MixOfStudentAndNonStudentTickets_ShouldReturnCorrectTotal()
+        {
+            // Arrange
+            Movie movie = new("Bliep bloep");
+            MovieScreening movieScreening = new(movie, DateTime.MinValue, 10);
+            Order order = new(321);
+
+            order.AddSeatReservation(new MovieTicket(movieScreening, true, false, 12, 12));
+            order.AddSeatReservation(new MovieTicket(movieScreening, false, false, 12, 12));
+
+            // Act
+            double totalPrice = order.CalculatePrice();
+
+            // Assert
+            Assert.That(totalPrice, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void CalculatePrice_MoreThanSixNonStudentTicketsOnWeekday_ShouldApplyDiscount()
+        {
+            // Arrange
+            Movie movie = new("Bliep bloep");
+            MovieScreening movieScreening = new(movie, DateTime.MinValue, 10);
+            Order order = new(321);
+
+            for (int i = 0; i < 7; i++)
+            {
+                order.AddSeatReservation(new MovieTicket(movieScreening, false, false, 12, 12));
+            }
+
+            // Act
+            double totalPrice = order.CalculatePrice();
+
+            // Assert
+            Assert.That(totalPrice, Is.EqualTo(36));
+        }
     }
 }
